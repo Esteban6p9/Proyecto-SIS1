43aa1fd99cc4785137292412887ca8cd34d15bb9
Leo-garnica <leo123leonardo907@gmail.com>
2025-07-28 13:43:34 -0400

    Implementación Login y manejo de rutas

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,16 @@
 <?php
 
-use App\Livewire\Settings\Appearance;
-use App\Livewire\Settings\Password;
-use App\Livewire\Settings\Profile;
+use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
+Route::get('/', [LoginController::class, 'showLoginForm'])->name('login');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::post('/login', [LoginController::class, 'login'])->name('login.post');
 
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+Route::get('/dashboard', function () {
+    return view('stats.dashboard');
+})->name('dashboard');
 
-    Route::get('settings/profile', Profile::class)->name('settings.profile');
-    Route::get('settings/password', Password::class)->name('settings.password');
-    Route::get('settings/appearance', Appearance::class)->name('settings.appearance');
-});
-
-require __DIR__.'/auth.php';
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
